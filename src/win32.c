3a1031b6b88ebeb14378341f2511b930b3b1ab0c
Fredrik Hultin <noname@nurd.se>
2013-10-09 14:24:38 +0200

    restructured files

--- src/win32.c
+++ src/win32.c
@@ -280,33 +280,3 @@ void shmipc_Destroy(shmipc** me)
 	free(*me);
 	*me = NULL;
 }
-
-shmipc_error shmipc_send_message(shmipc* me, const char* type, const char* message, size_t length, int timeout)
-{
-	char* buffer;
-	shmipc_error err = shmipc_acquire_buffer_w(me, &buffer, timeout);
-
-	if(err != SHMIPC_ERR_SUCCESS)
-		return err;
-
-	memcpy(buffer, message, length);
-	shmipc_return_buffer_w(me, &buffer, length, type);
-
-	return SHMIPC_ERR_SUCCESS;
-}
-
-shmipc_error shmipc_recv_message(shmipc* me, char* out_type, char* out_message, size_t* out_size, int timeout)
-{
-	const char* buffer;
-	size_t size;
-
-	shmipc_error err = shmipc_acquire_buffer_r(me, out_type, &buffer, &size, timeout);
-
-	if(err != SHMIPC_ERR_SUCCESS)
-		return err;
-
-	memcpy(out_message, buffer, size);
-	shmipc_return_buffer_r(me, &buffer); 
-
-	return SHMIPC_ERR_SUCCESS;
-}
